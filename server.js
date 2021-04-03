fe24e13b264230f63624a74ccab82fcb07221352
Stetco Nicolas <stetco.nicolas.t@gmail.com>
2021-04-03 14:57:31 +0200

    modified:   server.js 	modified:   social.sql

--- server.js
+++ server.js
@@ -10,18 +10,7 @@ const app = express();
 const port = 1337;
 let con;
 let saltRounds = 10;
-
-// const privateKey= fs.readFileSync("keys/private.key", "utf8");
-// const certificate= fs.readFileSync("keys/certificate.crt", "utf8");
-// const credentials ={"key": privateKey, "cert": certificate};
-
-// var httpsServer= https.createServer(credentials, app);
-// httpsServer.listen(port, '127.0.0.1', function(){
-//     console.log("Server running on port: "+port);
-// });
-
-
-
+ 
 // code 601 - query execution error
 ERRORS.create({
     code: 601,
@@ -72,56 +61,55 @@ app.post("/api/signUp", function (req, res, next) {
     let dataNascita = req.body.dataNascita;
     let password = req.body.password;
 
-        con.connect(function (err) {
-            if (err) {
-                console.log("Errore di connessione al DB");
-                error(req, res, new ERRORS.DB_CONNECTION({}));
-            } else {
+    con.connect(function (err) {
+        if (err) {
+            console.log("Errore di connessione al DB");
+            error(req, res, new ERRORS.DB_CONNECTION({}));
+        } else {
 
-                //controllo univocità mail
-                let queryMail = "SELECT idUtente FROM utenti WHERE mail='" + mail + "'";
-                con.query(queryMail, function (errCtrMail, resultMail) {
-                    if (errCtrMail) {
-                        console.log(errCtrMail);
-                        error(req, res, new ERRORS.QUERY_EXECUTE({}));
-                    } else {
+            //controllo univocità mail
+            let queryMail = "SELECT idUtente FROM utenti WHERE mail='" + mail + "'";
+            con.query(queryMail, function (errCtrMail, resultMail) {
+                if (errCtrMail) {
+                    console.log(errCtrMail);
+                    error(req, res, new ERRORS.QUERY_EXECUTE({}));
+                } else {
 
-                        if(resultMail.length==0){
+                    if (resultMail.length == 0) {
 
-                            //controllo univocità username
-                            let queryUser = "SELECT idUtente FROM utenti WHERE username='" + user + "'";
-                            con.query(queryUser, function (errCtrUser, resultUser) {
-                                if (errCtrUser) {
-                                    console.log(errCtrUser);
-                                    error(req, res, new ERRORS.QUERY_EXECUTE({}));
-                                } else {
-                                    if(resultUser.length==0)
-                                    {
-                                        bcrypt.hash(password, saltRounds, (err, hash) => {
-                                            if(err)
-                                                error(req, res, new ERRORS.HASH({}));
-                                            else {
-                                                //una volta assicurati che non vi sono altri utenti con quell' user e password, si procede alla registrazione
-                                                let queryString = "INSERT INTO utenti(username, password, nome, cognome, mail, foto, posizione, sesso, descrizione, dataNascita) VALUES ('" + user + "','" + hash + "','" + nome + "','" + cognome + "','" + mail + "','" + foto + "','" + posizione + "','" + sesso + "','" + descrizione + "'," + dataNascita + ")";
-                                                con.query(queryString, function (errQuery, result) {
-                                                    if (errQuery) {
-                                                        console.log(errQuery);
-                                                        error(req, res, new ERRORS.QUERY_EXECUTE({}));
-                                                    } else
-                                                        res.send("Signed up!");
-                                                });
-                                            }
-                                        });
-                                    }else
-                                        res.send("Username non disponibile, scegline un altro");
-                                }
-                            });
-                        }else
-                            res.send("Ti sei già registrato con questa mail");
-                    }
-                });
-            }
-        });
+                        //controllo univocità username
+                        let queryUser = "SELECT idUtente FROM utenti WHERE username='" + user + "'";
+                        con.query(queryUser, function (errCtrUser, resultUser) {
+                            if (errCtrUser) {
+                                console.log(errCtrUser);
+                                error(req, res, new ERRORS.QUERY_EXECUTE({}));
+                            } else {
+                                if (resultUser.length == 0) {
+                                    bcrypt.hash(password, saltRounds, (err, hash) => {
+                                        if (err)
+                                            error(req, res, new ERRORS.HASH({}));
+                                        else {
+                                            //una volta assicurati che non vi sono altri utenti con quell' user e password, si procede alla registrazione
+                                            let queryString = "INSERT INTO utenti(username, password, nome, cognome, mail, foto, posizione, sesso, descrizione, dataNascita) VALUES ('" + user + "','" + hash + "','" + nome + "','" + cognome + "','" + mail + "','" + foto + "','" + posizione + "','" + sesso + "','" + descrizione + "'," + dataNascita + ")";
+                                            con.query(queryString, function (errQuery, result) {
+                                                if (errQuery) {
+                                                    console.log(errQuery);
+                                                    error(req, res, new ERRORS.QUERY_EXECUTE({}));
+                                                } else
+                                                    res.send("Signed up!");
+                                            });
+                                        }
+                                    });
+                                } else
+                                    res.send("Username non disponibile, scegline un altro");
+                            }
+                        });
+                    } else
+                        res.send("Ti sei già registrato con questa mail");
+                }
+            });
+        }
+    });
 
 
 });
@@ -149,8 +137,8 @@ app.post("/api/login", function (req, res) {
                         console.log(errQuery);
                         res.send("Errore nella query: " + errQuery)
                     } else {
-                        // console.log(result);
-                        // res.send("logged");
+
+                        // result[0] perchè si da per scontata l'univocità della mail
                         bcrypt.compare(password, result[0].password, function (errCompare, resultCompare) {
                             if (errCompare)
                                 res.send("Errore compare hash");
@@ -166,6 +154,142 @@ app.post("/api/login", function (req, res) {
     });
 });
 
+
+
+app.post("/api/insertQuestion", function (req, res) {
+    con = mySql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "social"
+    });
+
+    let autore = req.body.autore;
+    let testo = req.body.testo;
+    let categoria = req.body.categoria;
+    let disponibile = 'T';
+
+    let queryString = "INSERT INTO domande(testoDomanda, data,categoria,disponibile, autore) VALUES ('" + testo + "',NOW()," + categoria + ",'" + disponibile + "'," + autore + ")";
+    con.query(queryString, function (errQuery, result) {
+        if (errQuery) {
+            //console.log(errQuery);
+            error(req, res, new ERRORS.QUERY_EXECUTE({}));
+        } else
+            res.send("Domanda inserita con successo");
+    });
+});
+
+
+
+app.post("/api/insertAnswer", function (req, res) {
+    con = mySql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "social"
+    });
+
+    let utente = req.body.utente;
+    let testo = req.body.testo;
+    let domanda = req.body.domanda;
+
+
+    let queryString = "INSERT INTO risposte(testoRisposta, data, domanda, utente) VALUES ('" + testo + "',NOW()," + domanda + "," + utente + ")";
+    con.query(queryString, function (errQuery, result) {
+        if (errQuery) {
+            //console.log(errQuery);
+            error(req, res, new ERRORS.QUERY_EXECUTE({}));
+        } else
+            res.send("Risposta inserita con successo");
+    });
+});
+
+
+
+app.post("/api/getQuestionByUser", function (req, res) {
+    con = mySql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "social"
+    });
+
+    let autore = req.body.autore;
+
+    let queryString = "SELECT * FROM domande WHERE autore= " + autore + " ";
+    con.query(queryString, function (errQuery, result) {
+        if (errQuery) {
+            console.log(errQuery);
+            error(req, res, new ERRORS.QUERY_EXECUTE({}));
+        } else
+            res.send(result);
+    });
+});
+
+
+
+app.post("/api/getQuestionsByCategory", function (req, res) {
+    con = mySql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "social"
+    });
+
+    let categoria = req.body.categoria;
+
+    let queryString = "SELECT * FROM domande WHERE categoria= " + categoria + " ";
+    con.query(queryString, function (errQuery, result) {
+        if (errQuery) {
+            console.log(errQuery);
+            error(req, res, new ERRORS.QUERY_EXECUTE({}));
+        } else
+            res.send(result);
+    });
+});
+
+
+app.post("/api/getAnswerByUser", function (req, res) {
+    con = mySql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "social"
+    });
+
+    let utente = req.body.utente;
+
+    let queryString = "SELECT * FROM risposte WHERE utente= " + utente + " ";
+    con.query(queryString, function (errQuery, result) {
+        if (errQuery) {
+            console.log(errQuery);
+            error(req, res, new ERRORS.QUERY_EXECUTE({}));
+        } else
+            res.send(result);
+    });
+});
+
+
+
+app.post("/api/getAnswerByQuestion", function (req, res) {
+    con = mySql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "social"
+    });
+
+    let domanda = req.body.domanda;
+
+    let queryString = "SELECT * FROM risposte WHERE domanda= " + domanda + " ";
+    con.query(queryString, function (errQuery, result) {
+        if (errQuery) {
+            console.log(errQuery);
+            error(req, res, new ERRORS.QUERY_EXECUTE({}));
+        } else
+            res.send(result);
+    });
+});
 
 
 
--- social.sql
+++ social.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.3
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Apr 03, 2021 alle 11:20
--- Versione del server: 10.4.14-MariaDB
--- Versione PHP: 7.4.11
+-- Creato il: Apr 03, 2021 alle 14:56
+-- Versione del server: 10.4.6-MariaDB
+-- Versione PHP: 7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -32,6 +33,14 @@ CREATE TABLE `categorie` (
   `nomeCategoria` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dump dei dati per la tabella `categorie`
+--
+
+INSERT INTO `categorie` (`idCategoria`, `nomeCategoria`) VALUES
+(1, 'Sport'),
+(2, 'Politica');
+
 -- --------------------------------------------------------
 
 --
@@ -43,9 +52,17 @@ CREATE TABLE `domande` (
   `testoDomanda` varchar(50) NOT NULL,
   `data` datetime NOT NULL,
   `categoria` int(11) NOT NULL,
-  `disponibile` tinyint(1) NOT NULL,
+  `disponibile` char(1) NOT NULL,
   `autore` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `domande`
+--
+
+INSERT INTO `domande` (`idDomanda`, `testoDomanda`, `data`, `categoria`, `disponibile`, `autore`) VALUES
+(1, 'Cosa ne pensi di Bettino Craxi?', '0000-00-00 00:00:00', 2, 'T', 5),
+(2, 'Chi è il GOAT in NBA?', '2021-04-03 14:14:28', 1, 'T', 6);
 
 -- --------------------------------------------------------
 
@@ -75,6 +92,13 @@ CREATE TABLE `risposte` (
   `data` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dump dei dati per la tabella `risposte`
+--
+
+INSERT INTO `risposte` (`idRisposta`, `domanda`, `utente`, `testoRisposta`, `data`) VALUES
+(1, 1, 5, 'un coglione', '2021-04-03 14:18:01');
+
 -- --------------------------------------------------------
 
 --
@@ -91,8 +115,18 @@ CREATE TABLE `utenti` (
   `foto` varchar(100) NOT NULL,
   `posizione` varchar(200) NOT NULL,
   `sesso` char(1) NOT NULL,
-  `descrizione` varchar(300) NOT NULL
+  `descrizione` varchar(300) NOT NULL,
+  `dataNascita` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `utenti`
+--
+
+INSERT INTO `utenti` (`idUtente`, `username`, `password`, `nome`, `cognome`, `mail`, `foto`, `posizione`, `sesso`, `descrizione`, `dataNascita`) VALUES
+(4, 'abbateeeee', '$2b$10$7pi1cpBAyTXBQ', 'andrea', 'abbate', 'abbate@gmail.com', 'abbate.jpg', '90', 'F', 'ti boccio', '0000-00-00'),
+(5, 'andreaAbbate', '$2b$10$L1PcpBjzHbGOy', 'andrea', 'abbate', 'andrea.abbate@gmail.com', 'abbate.jpg', '90', 'F', 'ti boccio', '0000-00-00'),
+(6, 'andreoneAbbate', '$2b$10$vt0OinojTB63L', 'andrea', 'abbate', 'andreone.abbate@gmail.com', 'abbate.jpg', '90', 'F', 'ti boccio', '0000-00-00');
 
 --
 -- Indici per le tabelle scaricate
@@ -142,13 +176,13 @@ ALTER TABLE `utenti`
 -- AUTO_INCREMENT per la tabella `categorie`
 --
 ALTER TABLE `categorie`
-  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT per la tabella `domande`
 --
 ALTER TABLE `domande`
-  MODIFY `idDomanda` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idDomanda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT per la tabella `messaggi`
@@ -160,13 +194,13 @@ ALTER TABLE `messaggi`
 -- AUTO_INCREMENT per la tabella `risposte`
 --
 ALTER TABLE `risposte`
-  MODIFY `idRisposta` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idRisposta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT per la tabella `utenti`
 --
 ALTER TABLE `utenti`
-  MODIFY `idUtente` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idUtente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Limiti per le tabelle scaricate
