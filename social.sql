c3e26079f2c9a981eeca2088646408347ed234b3
Andrea Tomatis <andreatomatis02.at@gmail.com>
2021-04-03 11:19:09 +0200

    ciao

---
+++ social.sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Mar 13, 2021 alle 08:48
+-- Versione del server: 10.4.14-MariaDB
+-- Versione PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `social`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `idCategoria` int(11) NOT NULL,
+  `nomeCategoria` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `domande`
+--
+
+CREATE TABLE `domande` (
+  `idDomanda` int(11) NOT NULL,
+  `testoDomanda` varchar(50) NOT NULL,
+  `data` datetime NOT NULL,
+  `categoria` int(11) NOT NULL,
+  `disponibile` tinyint(1) NOT NULL,
+  `autore` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `messaggi`
+--
+
+CREATE TABLE `messaggi` (
+  `idMessaggio` int(11) NOT NULL,
+  `testoMessaggio` varchar(500) NOT NULL,
+  `data` datetime NOT NULL,
+  `mittente` int(11) NOT NULL,
+  `destinatario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `risposte`
+--
+
+CREATE TABLE `risposte` (
+  `idRisposta` int(11) NOT NULL,
+  `domanda` int(11) NOT NULL,
+  `utente` int(11) NOT NULL,
+  `testoRisposta` varchar(250) NOT NULL,
+  `data` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `utenti`
+--
+
+CREATE TABLE `utenti` (
+  `idUtente` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `cognome` varchar(30) NOT NULL,
+  `mail` varchar(30) NOT NULL,
+  `foto` blob NOT NULL,
+  `posizione` varchar(200) NOT NULL,
+  `sesso` char(1) NOT NULL,
+  `descrizione` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`idCategoria`);
+
+--
+-- Indici per le tabelle `domande`
+--
+ALTER TABLE `domande`
+  ADD PRIMARY KEY (`idDomanda`),
+  ADD KEY `autoreForeignKey` (`autore`),
+  ADD KEY `categoriaForeignKey` (`categoria`);
+
+--
+-- Indici per le tabelle `messaggi`
+--
+ALTER TABLE `messaggi`
+  ADD PRIMARY KEY (`idMessaggio`),
+  ADD KEY `mittenteForeignKey` (`mittente`),
+  ADD KEY `destinatarioForeignKey` (`destinatario`);
+
+--
+-- Indici per le tabelle `risposte`
+--
+ALTER TABLE `risposte`
+  ADD PRIMARY KEY (`idRisposta`),
+  ADD KEY `autoreRispostaForeignKey` (`utente`),
+  ADD KEY `domandaForeignKey` (`domanda`);
+
+--
+-- Indici per le tabelle `utenti`
+--
+ALTER TABLE `utenti`
+  ADD PRIMARY KEY (`idUtente`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `domande`
+--
+ALTER TABLE `domande`
+  MODIFY `idDomanda` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `messaggi`
+--
+ALTER TABLE `messaggi`
+  MODIFY `idMessaggio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `risposte`
+--
+ALTER TABLE `risposte`
+  MODIFY `idRisposta` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `utenti`
+--
+ALTER TABLE `utenti`
+  MODIFY `idUtente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `domande`
+--
+ALTER TABLE `domande`
+  ADD CONSTRAINT `autoreForeignKey` FOREIGN KEY (`autore`) REFERENCES `utenti` (`idUtente`),
+  ADD CONSTRAINT `categoriaForeignKey` FOREIGN KEY (`categoria`) REFERENCES `categorie` (`idCategoria`);
+
+--
+-- Limiti per la tabella `messaggi`
+--
+ALTER TABLE `messaggi`
+  ADD CONSTRAINT `destinatarioForeignKey` FOREIGN KEY (`destinatario`) REFERENCES `utenti` (`idUtente`),
+  ADD CONSTRAINT `mittenteForeignKey` FOREIGN KEY (`mittente`) REFERENCES `utenti` (`idUtente`);
+
+--
+-- Limiti per la tabella `risposte`
+--
+ALTER TABLE `risposte`
+  ADD CONSTRAINT `autoreRispostaForeignKey` FOREIGN KEY (`utente`) REFERENCES `utenti` (`idUtente`),
+  ADD CONSTRAINT `domandaForeignKey` FOREIGN KEY (`domanda`) REFERENCES `domande` (`idDomanda`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
