1af727f92fce9aab13c0252f74b2a8f20011e133
Stetco Nicolas <stetco.nicolas.t@gmail.com>
2021-04-27 17:15:36 +0200

    modified:   appSocial/src/app/login/login.page.ts

--- appSocial/src/app/login/login.page.ts
+++ appSocial/src/app/login/login.page.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-
-
-
+import {HttpClient , HttpHeaders} from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -15,21 +12,19 @@ export class LoginPage implements OnInit {
   user:string="";
   loginResult:any;
   constructor(private http: HttpClient) { }
-
   ngOnInit() {
   }
-
   login() {
     if(this.user=="")
       this.errore="Inserisci l'username o l'indirizzo e-mail";
     else if(this.password=="")
       this.errore="Inserisci la password";
     else{
-
-      this.errore="bravissimo";
-      this.sendPostRequest("/api/login", JSON.stringify({mail:this.user,password:this.password})).subscribe(
+      //this.errore="bravissimo";
+      console.log(this.user, this.password);
+      const headers = new HttpHeaders().set('Content-Type', 'application/json');
+      this.sendPostRequest("/api/login", JSON.stringify({mail:this.user,password:this.password}), headers).subscribe(
         (data: any) => {
-
           console.log(data);
         },
         (error: any) => {
@@ -38,8 +33,8 @@ export class LoginPage implements OnInit {
       );
     }
   }
-  public sendPostRequest(servizio: string, datiBody:any) {
-
-    return this.http.post(this.ENDPOINT_SERVER + servizio, datiBody);
+  public sendPostRequest(servizio: string, datiBody:any, header:any) {
+    console.log(header);
+    return this.http.post(this.ENDPOINT_SERVER + servizio, datiBody, {headers: header});
   }
 }
